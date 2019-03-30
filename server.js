9887937edd55abbdf57b6d5f5cba796096afe5bc
vibakar <vibakar23@gmail.com>
2019-03-30 12:20:19 +0530

    Initial commit

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+var app = express();
+
+app.get('/', (req,res) => {
+	res.send({
+		message: "Welcome to nodejs!"
+	})
+});
+
+
+app.listen(3000, ()=> {
+	console.log("Application is running on port 3000");
+})
